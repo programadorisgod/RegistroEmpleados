d595b8cc87fe86fc07683ed55a30970c9b71ee7a
programadorisgod <catapiascastro@gmail.com>
2022-11-23 15:56:15 -0500

    Agregar archivos de proyecto.

---
+++ Datos/ConexionOracle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oracle.ManagedDataAccess.Client;
+
+namespace Datos
+{
+    public class ConexionOracle
+    {
+        private string cadena = "Data Source=(DESCRIPTION =" +
+           "(ADDRESS = (PROTOCOL = TCP)(HOST =127.0.0.1)(PORT = 1521))" +
+           "(CONNECT_DATA =" +
+           "(SERVER = DEDICATED)" +
+           "(SERVICE_NAME = XE)));" +
+           "User Id=jerson;Password=4090;";
+        private OracleConnection cnn = new OracleConnection();
+        
+        public ConexionOracle()
+        {
+            cnn.ConnectionString = cadena;
+        }
+
+    }
+    
+   
+}
